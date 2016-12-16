f9136f81ac6a63e68f7b3366728aecb359c8418a
Ryan Shea <ryan@shea.io>
2016-12-15 23:50:05 -0500

    simplify app to 30 lines

--- hello-world/public/app.js
+++ hello-world/public/app.js
@@ -2,13 +2,9 @@ $(document).ready(function() {
   var identityProviderURL = "https://blockstack-dashboard.firebaseapp.com/auth"
   var nameResolverURL = "https://api.blockstack.com/v1/users/"
   var authAgent = new AuthAgent(identityProviderURL, nameResolverURL)
-  window.authAgent = authAgent
-
   $('#logout-button').click(authAgent.logout);
-  $('#login-button').click(function() {
-    authAgent.requestAuthentication()
-  });
-
+  $('#login-button').click(function() { authAgent.requestAuthentication() });
+  
   function showProfile(username, profile) {
     var person = new Person(profile)
     $('.heading-name').html(person.name())
@@ -16,19 +12,17 @@ $(document).ready(function() {
     $('#section-1').hide()
     $('#section-2').show()
   }
-
+  
   function runApp() {
     if (authAgent.isUserLoggedIn()) {
       var blockstackData = JSON.parse(localStorage.getItem(authAgent.localStorageKeyName))
       showProfile(blockstackData.username, blockstackData.profile)
-    } else {
-      if (authAgent.getAuthResponseToken()) {
-        var authResponseToken = authAgent.getAuthResponseToken()
-        authAgent.loadUser(authResponseToken, function(username, profile) {
-          authAgent.recordSession(authResponseToken, username, profile)
-          window.location = authAgent.currentHost
-        })
-      }
+    } else if (authAgent.getAuthResponseToken()) {
+      var authResponseToken = authAgent.getAuthResponseToken()
+      authAgent.loadUser(authResponseToken, function(username, profile) {
+        authAgent.recordSession(authResponseToken, username, profile)
+        window.location = authAgent.currentHost
+      })
     }
   }
 
