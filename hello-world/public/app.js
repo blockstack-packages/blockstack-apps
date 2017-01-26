9ed23d369d38a934a3571cdf98b3614a2850545f
Ryan Shea <ryan@shea.io>
2017-01-25 21:17:26 -0500

    update js, html, and css of the hello, blockstack app

--- hello-world/public/app.js
+++ hello-world/public/app.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-  var identityProviderURL = "https://blockstack-dashboard.firebaseapp.com/auth",
-      nameResolverURL = "https://api.blockstack.com/v1/users/",
-      currentHost = window.location.origin
-  var blockstack = new BlockstackAuth.AuthAgent(identityProviderURL, nameResolverURL);
-  $('#logout-button').click(function() { blockstack.logout() })
-  $('#login-button').click(function() { blockstack.requestLogin() })
-  
-  function showProfile(username, profile) {
-    var person = new Person(profile)
-    $('.heading-name').html(person.name())
-    $('#avatar-image').attr("src", person.avatarUrl())
-    $('#section-1').hide()
-    $('#section-2').show()
-  }
-  
-  if (blockstack.isUserLoggedIn()) { // If the user is logged in, get the session
-    blockstack.loadSession(function(session) {
-      showProfile(session.username, session.profile)
-    })
-  } else if (blockstack.isLoginPending()) { // If there's an auth token, login the user
-    blockstack.completeLogin(function(session) {
-      window.location = currentHost
-    })
-  }
+var identityProviderURL = "https://blockstack-dashboard.firebaseapp.com/auth"
+var nameResolverURL = "https://api.blockstack.com/v1/users/"
+var blockstack = new BlockstackAuth.AuthAgent(identityProviderURL, nameResolverURL);
+var currentHost = window.location.origin
+$('#login-button').click(function() { blockstack.requestLogin() })
+$('#logout-button').click(function() { blockstack.logout() })
+
+function showProfile(username, profile) {
+  var person = new Person(profile)
+  $('#heading-name').html(person.name())
+  $('#avatar-image').attr("src", person.avatarUrl())
+  $('#section-1').hide()
+  $('#section-2').show()
+}
+
+if (blockstack.isUserLoggedIn()) { // User logged in? Get the session.
+  blockstack.loadSession(function(session) {
+    showProfile(session.username, session.profile)
+  })
+} else if (blockstack.isLoginPending()) { // Auth token present? Login the user.
+  blockstack.completeLogin(function(session) {
+    window.location = currentHost
+  })
+}
 })
